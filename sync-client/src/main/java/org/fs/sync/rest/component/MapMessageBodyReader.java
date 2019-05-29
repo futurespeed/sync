cfe8de7647b648cbe6b2e0fd1184dbaf04a7de33
gaojingxin <gaojingxin@example.com>
2019-05-29 20:01:55 +0800

    update

--- sync-client/src/main/java/org/fs/sync/rest/component/MapMessageBodyReader.java
+++ sync-client/src/main/java/org/fs/sync/rest/component/MapMessageBodyReader.java
@@ -1,7 +1,6 @@
 package org.fs.sync.rest.component;
 
 import com.alibaba.fastjson.JSON;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.commons.io.IOUtils;
 
 import javax.ws.rs.WebApplicationException;
@@ -9,6 +8,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Provider;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
@@ -23,8 +23,8 @@ public class MapMessageBodyReader implements MessageBodyReader {
 
     @Override
     public Object readFrom(Class type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
-        ByteOutputStream bo = new ByteOutputStream();
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
         IOUtils.copy(entityStream, bo);
-        return JSON.parseObject(new String(bo.getBytes(), 0, bo.getCount(), "UTF-8"), java.util.Map.class);
+        return JSON.parseObject(new String(bo.toByteArray(), "UTF-8"), java.util.Map.class);
     }
 }
